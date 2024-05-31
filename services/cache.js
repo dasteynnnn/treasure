cd58583c7a2c9359c3defb7dd209eb4ed85434fd
Dustin John Alanano <dustinjohnvaler.alanano@gmail.com>
2024-05-31 12:09:42 +0800

    fix redis 053124

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,7 @@
-const redis = require('redis');
+const Redis = require('ioredis');
 
 const redisUrl = process.env.UPSTASH_REDIS_REST_URL || "redis://127.0.0.1:6379"
-const redisClient = redis.createClient ({
-    url : redisUrl
-});
+const redisClient = new Redis(redisUrl);
 
 redisClient.on("error", function(err) {
     throw err;
